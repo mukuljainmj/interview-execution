f89df27f9e3175d352dace223705431469ca1f4b
mukuljainmj <mukuljain0295@gmail.com>
2025-04-23 17:04:06 +0530

    interview execution setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function App() {
+  return <div className="text-3xl font-bold underline">App</div>;
+}
+
+root.render(<App></App>);
